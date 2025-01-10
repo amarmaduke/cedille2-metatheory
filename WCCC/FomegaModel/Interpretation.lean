5e678692864f5ffffe0bc8b90da40b4eae93faa3
Andrew Marmaduke <marmaduke.andrew@gmail.com>
2025-01-10 12:14:53 -0600

    fix up interp

--- WCCC/FomegaModel/Interpretation.lean
+++ WCCC/FomegaModel/Interpretation.lean
@@ -12,26 +12,24 @@ namespace FomegaModel
     else 𝒱 B
   | _ => .none
 
-  -- 𝓉
+
+  def g0 (ℓ : Nat) : Term := .bound .kind (ℓ - 2)
+  def gBot (ℓ : Nat) : Term := .bound .type (ℓ - 1)
+
   @[simp]
-  def 𝒯 : Term -> Term
+  def 𝒯 (ℓ : Nat) : Term -> Term
   | □ | ★ => .bound .kind 0
   | .bound .kind x => .bound .kind x
   | .all _ A B =>
-    if A.classify = .kind then
-      .all mf (𝒱 A) (.all mf (𝒯 A) (𝒯 B))
-    else if A.classify = .type then
-      .all mf ★ (.all mf (𝒯 A) (𝒯 B))
-    else .none
+    if A.classify = .kind then ∀f[𝒱 A] ∀f[𝒯 (ℓ + 1) A] 𝒯 (ℓ + 2) B
+    else ∀f[★] ∀f[𝒯 (ℓ + 1) A] 𝒯 (ℓ + 2) B
   | .lam .type A t =>
-    if A.classify = .kind then .lam mf (𝒱 A) (𝒯 t)
-    else if A.classify = .type then 𝒯 t
-    else .none
+    if A.classify = .kind then λf[𝒱 A] 𝒯 (ℓ + 1) t
+    else 𝒯 ℓ t
   | .app .type f a =>
-    if a.classify = .type then .app mf (𝒯 f) (𝒯 a)
-    else if a.classify = .term then 𝒯 f
-    else .none
-  | .prod A B => Fomega.PairTy A B
+    if a.classify = .type then (𝒯 ℓ f) `@f (𝒯 ℓ a)
+    else  𝒯 ℓ f
+  | .prod A B => .spair A B
   | .eq _ _ _ => Fomega.IdTy
   | _ => .none
 
@@ -39,45 +37,47 @@ namespace FomegaModel
   def γ : Ctx -> Ctx
   | [] => ★ :: Fomega.Bot :: []
   | .cons A Γ =>
-    if A.classify = .kind then (𝒯 A) :: (𝒱 A) :: (γ Γ)
-    else (𝒯 A) :: (𝒯 A) :: (γ Γ)
-
-  def g0 (ℓ : Nat) : Term := .bound .kind (ℓ - 2)
-  def gBot (ℓ : Nat) : Term := .bound .type (ℓ - 1)
+    let ℓ := Γ.length
+    if A.classify = .kind then (𝒯 ℓ A) :: (𝒱 A) :: (γ Γ)
+    else (𝒯 ℓ A) :: (𝒯 ℓ A) :: (γ Γ)
 
   @[simp]
   def canon (ℓ : Nat) : Term -> Term
   | ★ => g0 ℓ
   | .all mf A B =>
-    if B.classify = .kind then .lam mf A (canon ℓ B)
+    if B.classify = .kind then λf[A] (canon ℓ B)
     else (gBot ℓ) `@τ (.all mf A B)
   | B => (gBot ℓ) `@τ B
 
   @[simp]
-  def 𝓉 (ℓ : Nat) : Term -> Term -> Term
-  | ★, _ => c zr
-  | .bound .type x, _ => .bound .kind (2*x)
-  | .bound .kind x, _ => .bound .type (2*x + 1)
-  | .all _ A B, _ =>
+  def 𝓉 (ℓ : Nat) : Term -> Term
+  | ★ => c zr
+  | .bound .type x => .bound .kind (2*x)
+  | .bound .kind x => .bound .type (2*x + 1)
+  | .all _ A B =>
     if A.classify = .kind then
-      c (∀f[zr] ∀f[zr] zr) `@f 𝓉' A `@f (𝓉' B) β[c (𝒯 A)] β[c (𝒱 A)]
+      c (∀f[zr] ∀f[zr] zr) `@f 𝓉 ℓ A `@f (𝓉 ℓ B) β[c (𝒯 ℓ A)] β[c (𝒱 A)]
     else
-      c (∀f[zr] ∀f[zr] zr) `@f 𝓉' A `@f (𝓉' B) β[c (𝒯 A)] β[zr]
-  | .lam _ A t, _ =>
+      c (∀f[zr] ∀f[zr] zr) `@f 𝓉 ℓ A `@f (𝓉 ℓ B) β[c (𝒯 ℓ A)] β[zr]
+  | .lam _ A t =>
     if A.classify = .kind then
-      (λf[zr] λf[𝒱 A] λf[𝒯 A] 𝓉' t) `@f 𝓉' A
+      (λf[zr] λf[𝒱 A] λf[𝒯 ℓ A] 𝓉 ℓ t) `@f 𝓉 ℓ A
     else
-      (λf[zr] λf[★] λf[𝒯 A] 𝓉' t) `@f 𝓉' A
-  | .app _ f a, _ =>
-    if a.classify = .type then 𝓉' f `@f 𝒯 a `@f 𝓉' a
-    else 𝓉' f `@f zr `@f 𝓉' a
-  | .prod A B, _ => sorry
-  | .pair _ B' t s, .prod A B => sorry
-  | .fst t, _ => sorry
-  | _, _ => .none
+      (λf[zr] λf[★] λf[𝒯 ℓ A] 𝓉 ℓ t) `@f 𝓉 ℓ A
+  | .app _ f a =>
+    if a.classify = .type then 𝓉 ℓ f `@f 𝒯 ℓ a `@f 𝓉 ℓ a
+    else 𝓉 ℓ f `@f zr `@f 𝓉 ℓ a
+  | .prod A B => c (∀f[zr] ∀f[zr] zr) `@f 𝓉 ℓ A `@f (𝓉 ℓ B) β[c (𝒯 ℓ A)]
+  | .pair _ B t s => (λf[zr] .spair (𝓉 ℓ t) (𝓉 ℓ s)) `@f (𝓉 ℓ B) β[𝓉 ℓ t]
+  | .fst t => .fst (𝓉 ℓ t)
+  | .snd t => .snd (𝓉 ℓ t)
+  | .eq A a b => c (∀f[zr] ∀f[𝒯 ℓ A] ∀f[𝒯 ℓ A] zr) `@f 𝓉 ℓ A `@f 𝓉 ℓ a `@f 𝓉 ℓ b
+  | .refl t => sorry
+  | .subst Pr e => sorry
+  | .phi a b e => sorry
+  | .conv _ A t => (λf[zr] .id (𝓉 ℓ t)) `@f 𝓉 ℓ A
+  | _ => .none
   where
-    𝓉' : Term -> Term
-    | t => 𝓉 ℓ t .none
     c : Term -> Term
     | t => canon ℓ t
     zr := g0 ℓ
