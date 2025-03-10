556788cef6a90c1b435fce5c8e23f3c2a3bcb4c7
Andrew Marmaduke <marmaduke.andrew@gmail.com>
2025-03-10 10:33:04 -0500

    finish substitution

--- Cedille2/Basic/Substitution.lean
+++ Cedille2/Basic/Substitution.lean
@@ -71,36 +71,66 @@ namespace Cedille2.Proof
     simp; constructor; apply ih1 h2 h3 j2
     replace ih2 := @ih2 (^σ) ([σ]A :: Δ) (lift_subst_rename _ h2) (lift_subst_replace lem1 h3) lem2
     simp at ih2; exact ih2
-  case lam Γ A K1 K2 B t _j1 _j2 _j3 ih1 ih2 ih3 => sorry
-    -- have lem1 := ih1 h2 h3 j2
-    -- have lem2 : ⊢ ([σ]A :: Δ) := by constructor; apply j2; apply lem1
-    -- simp; constructor; simp at *; apply ih1 h2 h3 j2
-    -- replace ih2 := @ih2 (^σ) ([σ]A :: Δ) (lift_subst_rename _ h2)  (lift_subst_replace lem1 h3) lem2
-    -- simp at ih2; exact ih2
-    -- replace ih3 := @ih3 (^σ) ([σ]A :: Δ) (lift_subst_rename _ h2)  (lift_subst_replace lem1 h3) lem2
-    -- simp at ih3; exact ih3
+  case lam A m K B t q1 q2 q3 q4 ih1 ih2 ih3 =>
+    have lem1 := ih1 h2 h3 j2
+    have lem2 : ⊢ ([σ]A :: Δ) := by constructor; apply j2; apply lem1
+    replace ih2 := @ih2 (^σ) ([σ]A :: Δ) (lift_subst_rename _ h2) (lift_subst_replace lem1 h3) lem2
+    replace ih3 := @ih3 (^σ) ([σ]A :: Δ) (lift_subst_rename _ h2) (lift_subst_replace lem1 h3) lem2
+    simp at ih2; simp at ih3
+    simp; constructor; apply lem1
+    apply ih2; apply ih3
+    intro h v1 v2 τ
+    have lem := q4 h
+    rw [erase_subst]; simp
+    rw [Subst.map_S_compose_left erase_rename_commute]; simp
+    apply lem
   case app _j1 _j2 j3 ih1 ih2 =>
-    simp; constructor; simp at ih1; apply ih1 h2 h3 j2; apply ih2 h2 h3 j2
+    simp; constructor; simp at ih1
+    apply ih1 h2 h3 j2; apply ih2 h2 h3 j2
     subst j3; simp
-  case inter_ty ih1 ih2 => sorry
-    -- simp; constructor; apply ih1 h2 h3 j2; apply ih2 h2 h3 j2
-  case inter ih1 ih2 ih3 ih4 => sorry
-    -- simp; constructor; apply ih1 h2 h3 j2; apply ih2 h2 h3 j2
-    -- apply ih3 h2 h3 j2; apply ih4 h2 h3 j2
+  case inter_ty A B _ _ ih1 ih2 =>
+    have lem1 := ih1 h2 h3 j2
+    have lem2 : ⊢ ([σ]A :: Δ) := by constructor; apply j2; apply lem1
+    replace ih2 := @ih2 (^σ) ([σ]A :: Δ) (lift_subst_rename _ h2) (lift_subst_replace lem1 h3) lem2
+    simp at *; constructor; apply lem1; apply ih2
+  case inter t A B s g1 g2 q1 q2 q3 q4 q5 ih1 ih2 ih3 ih4 =>
+    have lem1 := ih2 h2 h3 j2
+    have lem2 : ⊢ ([σ]A :: Δ) := by constructor; apply j2; apply lem1
+    replace ih3 := @ih3 (^σ) ([σ]A :: Δ) (lift_subst_rename _ h2) (lift_subst_replace lem1 h3) lem2
+    simp at ih3; replace ih4 := ih4 h2 h3 j2
+    simp at ih4
+    simp; constructor; apply ih1 h2 h3 j2
+    apply lem1; apply ih3; simp; apply ih4
+    rw [erase_subst, erase_subst]
+    apply Erased.convb_subst; apply q5
   case fst ih =>
     simp; constructor; apply ih h2 h3 j2
-  case snd ih => sorry
-    -- simp; constructor; apply ih h2 h3 j2
-  case eq ih => sorry
-    -- simp; constructor; apply ih h2 h3 j2
-  case refl => sorry
-    -- simp; constructor; apply ih h2 h3 j2
-  case subst => sorry
-  case phi => sorry
+  case snd A B B' j1 j2 ih =>
+    simp; apply @Judgment.snd _ _ _ ([^σ]B)
+    apply ih h2 h3 j2; simp [*]
+  case eq ih1 ih2 ih3 =>
+    simp; constructor; apply ih1 h2 h3 j2
+    apply ih2 h2 h3 j2; apply ih3 h2 h3 j2
+  case refl ih1 ih2 =>
+    simp; constructor; apply ih1 h2 h3 j2
+    apply ih2 h2 h3 j2
+  case subst ih1 ih2 ih3 ih4 =>
+    replace ih3 := ih3 h2 h3 j2; simp at ih3
+    simp; constructor; apply ih1 h2 h3 j2
+    apply ih2 h2 h3 j2; simp; apply ih3
+    apply ih4 h2 h3 j2
+  case phi ih1 ih2 ih3 =>
+    replace ih2 := ih2 h2 h3 j2; simp at ih2
+    simp; constructor; apply ih1 h2 h3 j2
+    apply ih2; apply ih3 h2 h3 j2
   case iconv Γ' t' A' B K _j1 _j2 j3 ih1 ih2 =>
     constructor; apply ih1 h2 h3 j2; apply ih2 h2 h3 j2
     apply Red.Conv.subst_same j3
-  case econv => sorry
+  case econv j ih1 ih2 =>
+    simp; apply Judgment.econv; apply ih1 h2 h3 j2
+    apply ih2 h2 h3 j2
+    rw [erase_subst, erase_subst]
+    apply Erased.convb_subst; apply j
 
   theorem beta : (A::Γ) ⊢ b : B -> Γ ⊢ t : A -> Γ ⊢ (b β[t]) : (B β[t]) := by
   simp; intro j1 j2
